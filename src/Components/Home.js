2d1d4c96726c6866656cad26ebc4566a2f73b259
Jafer Haider <itsjafer@gmail.com>
2024-09-02 19:21:15 -0500

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -17,9 +17,7 @@ class Home extends Component {
           and I'm a Software Engineer at Google. In 2021, I graduated as a double-degree student from the University of Waterloo and Wilfrid Laurier University with degrees in Computer Science and Business Administration, respectively.</p>
         
         <p>
-          In my free time, I&apos;m a big fan of video games, movies, and TV Shows (for real, I even have a
-          <a href="/#/favourites"> list</a>
-          ). I also generally enjoy working on side projects that make my life easier. In particular, there's a good chance you've come across my website because of my popular <a href="/#/parser">resume parser project</a>.
+          I also generally enjoy working on side projects that make my life easier. In particular, there's a good chance you've come across my website because of my popular <a href="/#/parser">resume parser project</a>.
         </p>
         
         <p>If you're looking to connect to talk about experiences, education, connect about an opportunity, or just hangout, feel free to send me an email or connect on linkedin!
