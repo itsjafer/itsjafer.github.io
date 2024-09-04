5ab3aa043bd3f431aacf11b0cbfb769b57825d35
Jafer Haider <itsjafer@gmail.com>
2024-09-04 02:34:57 +0000

    remove chai

--- src/Main.js
+++ src/Main.js
@@ -15,7 +15,6 @@ import Schedule from './Components/Schedule';
 import ShowPredictor from './Components/ShowPredictor';
 import ReverseSplit from './Components/ReverseSplit';
 import Parser from './Components/Parser';
-import Chai from './Components/Chai';
 import {Blog, BlogEntry} from './Components/Blog';
 import Trader from './Components/Trader';
 import Loonie from './Components/Loonie/Loonie';
@@ -63,7 +62,6 @@ class Main extends Component {
             <Route exact path="/technical" component={withTracker(Technical)} />
             <Route exact path="/resume" component={withTracker(Technical)} />
             <Route path="/favourites" component={withTracker(Favourites)} />
-            <Route path="/chai" component={withTracker(Chai)} />
             <Route exact path="/blog" component={withTracker(Blog)} />
             <Route exact path="/blog/:file/" component={withTracker(BlogEntry)} />
             <Route path="/reversesplit" component={withTracker(ReverseSplit)} />
